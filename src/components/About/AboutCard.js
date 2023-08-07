821894418795a51aed4b6f41d8d7173c1db95679
Habib Syuhada <habibsumedang@gmail.com>
2023-08-07 16:36:30 +0700

    about complete, project on progress

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,18 +8,19 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
-            <br /> I am a final year student pursuing an Integrated MSc (IMSc)
-            in Maths and Computing at BIT Mesra.
+						Hello, my name is <span className="purple">Habib Syuhada </span>
+            from <span className="purple"> Batam, Indonesia.</span>
+            <br /> I am a graduate of D3 Computer Engineering from the Batam State Polytechnic.
             <br />
-            Additionally, I am currently employed as a software developer at
-            Juspay.
+            Currently, I am working as an IT Senior Programmer at PT SMOE.
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
           </p>
           <ul>
+            <li className="about-activity">
+              <ImPointRight /> Reading Books
+            </li>
             <li className="about-activity">
               <ImPointRight /> Playing Games
             </li>
@@ -31,10 +32,10 @@ function AboutCard() {
             </li>
           </ul>
 
-          <p style={{ color: "rgb(155 126 172)" }}>
+          {/* <p style={{ color: "rgb(155 126 172)" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Soumyajit</footer> */}
         </blockquote>
       </Card.Body>
     </Card>
