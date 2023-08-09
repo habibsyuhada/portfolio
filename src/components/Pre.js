93f32c3088c44f2febd448e2e5053529306e1401
Habib Syuhada <habibsumedang@gmail.com>
2023-08-09 15:16:53 +0700

    loading change

--- src/components/Pre.js
+++ src/components/Pre.js
@@ -1,6 +1,7 @@
 import React from "react";
+import { ImSpinner2 } from "react-icons/im";
 function Pre(props) {
-  return <div id={props.load ? "preloader" : "preloader-none"}></div>;
+  return <div id={props.load ? "preloader" : "preloader-none"} ><ImSpinner2 className="loader-icon" /></div>;
 }
 
 export default Pre;
